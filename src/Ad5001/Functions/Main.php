734693dda12b75bb9ab1368a9e06f7934b802517
Ad5001 <ad5001yt@gmail.com>
2016-04-21 11:34:55 +0200

    Updates!

--- src/Ad5001/Functions/Main.php
+++ src/Ad5001/Functions/Main.php
@@ -31,40 +31,43 @@ class Main extends PluginBase implements Listener{
 					return false;
 				} else {
 					 $cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
-					  $default = ["op", "tell {sender} This is default command, modify it with /function setc <function> <Command number> <command...>","nothink", "nothink", "nothink", "nothink"];
-					  $cfg->set($args[1], $default);
+					  $default = ["tell {sender} This is default command, modify it with /function setc <function> <Command number> <command...>","nothink", "nothink", "nothink", "nothink"];
+					  $cfg->set("/".$args[1], $default);
                       $cfg->save();
-					  $sender->sendMessage("§4§l[Functions] Function " . $args[1] . " has been created! You can edit it on the config or by doing /function sc <function> <command number> <command...>.");
+					  $this->reloadConfig();
+					  $sender->sendMessage("§4§l[Functions]§r§4 Function " . $args[1] . " has been created! You can edit it on the config or by doing /function sc <function> <command number> <command...>.");
 			    }
 					 return true;
 					 break;
-				case "setcmd":
-				  $sender->sendMessage("§4§l[Functions] Use /function sc <function> <command number> <command>! It's more speedy but your way work too");
-				case "sc":
-				#if($cfg->get($args[1] ==! null) {
-					$cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+				case "addcmd":
+				  $sender->sendMessage("§4§l[Functions] Use /function ac <function> <command>! It's more speedy but your way work too");
+				case "ac":
+				$cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+				if(is_array($cfg->get("/".$args[1]))) {
 				     unset($args[0]);
 					 $funcname = $args[1];
 					 unset($args[1]);
-				     $cmdid = $args[2];
-					 unset($args[2]);
-					 $funccmds = $cfg->get($funcname);
-					 $funccmds[$cmdid + 1] = implode(" ", $args);
-				     $cfg->set($funcname, $funccmds);
+					 $funccmds = $cfg->get("/".$funcname);
+					 array_push($funccmds, implode(" ", $args));
+				     $cfg->set("/".$funcname, $funccmds);
 					 $cfg->save();
-					 $sender->sendMessage("Command " . $cmdid . " for function " . $funcname . " has been change to: /" . implode(" ", $args));
-				#} else {
-				#	$sender->sendMessage("§4§l[Functions] Function " . $args[1] . " not found. Create it with /function c " . $args[1]);
-				#}
+					 $sender->sendMessage("§4§l[Functions]§r§4 Command ". implode(" ", $args)."  for function " . $funcname . " has been added!");
+					 $this->reloadConfig();
+				} else {
+					$sender->sendMessage("§4§l[Functions]§r§4 Function " . $args[1] . " not found. Create it with /function c " . $args[1]);
+				}
 					 return true;
 					 break;
 				case "rc":
 				case "removecmd":
-				     $cfg = new Config($this->getDataFolder() . "config.yaml", Config::YAML);
-					 $func = $cfg->get($args[1]);
-					 $oldcmd = $func[$args[0]+1];
-					 unset($func[$args[0]+1]);
+				     $cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+					 $func = $cfg->get("/".$args[1]);
+					 $oldcmd = $func[$args[2]];
+					 $func[$args[2]] = "nothink";
 				     $sender->sendMessage("§4§l[Functions] Removed command (" . $oldcmd . ") of function " . $args[1]);
+					 $cfg->set("/".$args[1], $func);
+					 $cfg->save();
+					 $this->reloadConfig();
 					 return true;
 					 break;
 				case "read":
@@ -84,12 +87,12 @@ class Main extends PluginBase implements Listener{
 					 return true;
 					 break;
 			   default:
-				      $sender->sendMessage("§4§l[Functions] Help for Function: \n- /function create <function>:§6 Create a function \n- /function sc <function> <command id> <command>:§6 Modify a command a function \n- /function rc <function> <command id> <command>:§6 Remove a command from a function");
+				      $sender->sendMessage("§4§l[Functions]§r§4 Help for Function: \n- /function create <function>:§6 Create a function \n- /function sc <function> <command id> <command>:§6 Modify a command a function \n- /function rc <function> <command id> <command>:§6 Remove a command from a function");
 					  return true;
 					  break;
 			}
 			} else {
-				$sender->sendMessage("§4§l[Functions] Help for Function: \n- /function create <function>:§6 Create a function \n- /function sc <function> <command id> <command>:§6 Modify a command a function \n- /function rc <function> <command id> <command>:§6 Remove a command from a function");
+				$sender->sendMessage("§4§l[Functions]§r§4 Help for Function: \n- /function create <function>:§6 Create a function \n- /function sc <function> <command id> <command>:§6 Modify a command a function \n- /function rc <function> <command id> <command>:§6 Remove a command from a function");
 			}
 			return true;
 			break;
@@ -99,76 +102,41 @@ class Main extends PluginBase implements Listener{
 		    $cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
 			$args = $event->getMessage();
 			$sender = $event->getPlayer();
-			$cmds = explode(" ", $args);
-			$args = explode("/", $args[0]);
+			$args = explode(" ", $args);
 			$cmds = $cfg->get($args[0]);
-		    if($cmds[0] ===! "op" or !$cmds[0] ===! "perm" or $cmds[0] ===! "true" or $cmds[0] ===! "console"){
+		    if(!is_array($cmds)){
 			} else {
-				$event->setCancelled();
 				$funcname = $args[0];
-				switch($cmds[0]) {
-					case "op":
-					    if($sender->isOp()) {
-							unset($cmds[0]);
 							foreach($cmds as $cmd) {
 						          if ($cmd === "nothink") {
 								  } else {
+									  $cmd = str_ireplace("{sender}", $sender->getName(), $cmd);
+									  $cmd = str_ireplace("{level}", $sender->getLevel()->getName(), $cmd);
+									  $cmd = str_ireplace("{x}", $sender->x, $cmd);
+									  $cmd = str_ireplace("{y}", $sender->y, $cmd);
+									  $cmd = str_ireplace("{z}", $sender->z, $cmd);
+									  $cmd = str_ireplace("{yaw}", $sender->yaw, $cmd);
+									  $cmd = str_ireplace("{pitch}", $sender->pitch, $cmd);
+									  if(!isset($args[1])) {
+                                       $args[1] = null;
+									  }
+									  $cmd = str_ireplace("{args[0]}", $args[1], $cmd);
+									  if(!isset($args[2])) {
+                                       $args[2] = null;
+									  }
+									  $cmd = str_ireplace("{args[1]}", $args[2], $cmd);
+									  if(!isset($args[3])) {
+                                       $args[3] = null;
+									  }
+									  $cmd = str_ireplace("{args[2]}", $args[3], $cmd);
+									  if(!isset($args[4])) {
+                                       $args[4] = null;
+									  }
+									  $cmd = str_ireplace("{args[3]}", $args[4], $cmd);
 									  $this->getServer()->dispatchCommand($sender, $cmd);
 								  }
 				           }
-						} else {
-								$sender->sendMessage("§4You must be OP to use this command");
-						}
-						return true;
-						break;
-					case "perm":
-					    if($sender->hasPermission("func.use.func")) {
-							unset($cmds[0]);
-							foreach($cmds as $cmd) {
-						          if ($cmd === "nothink") {
-								  } else {
-									  $this->getServer()->dispatchCommand($sender, implode("", $cmd));
-								  }
-				           }
-						} else {
-								$sender->sendMessage("§4You don't have the permission to use this command");
-						}
-					case "true":
-							unset($cmds[0]);
-							foreach($cmds as $cmd) {
-						          if ($cmd === "nothink") {
-								  } else {
-									  $this->getServer()->dispatchCommand($sender, implode("", $cmd));
-								  }
-				           }
-					case "console":
-					    if(!$sender instanceof Player) {
-							unset($cmds[0]);
-							foreach($cmds as $cmd) {
-						          if ($cmd === "nothink") {
-								  } else {
-									  $this->getServer()->dispatchCommand($sender, implode("", $cmd));
-								  }
-				           }
-						} else {
-								$sender->sendMessage("§4You must run this in console");
-						}
-					default:
-					     $sender->sendMessage("§4§l[Functions] Default value not recognized. Changed to op.");
-						 $cmds = $cfg->get($args[0]);
-						 $cmds[0] = "op";
-						 $cfg->set($args[0], $cmds);
-					     $cfg->save();
-					     if($sender->isOp()) {
-							unset($cmds[0]);
-							foreach($cmds as $cmd) {
-						          if ($cmd === "nothink") {
-								  } else {
-									  $this->getServer()->dispatchCommand($sender, implode("", $cmd));
-								  }
-				           }
-						}
-				}
+						   $event->setCancelled();
 			}
 	}
   }
