e9be85fd7de42b82f94c8365dd003bf9f8e45a78
applqpak <applqpak@gmail.com>
2016-10-22 13:10:28 +1100

    Code cleanup

--- src/Ad5001/Functions/Main.php
+++ src/Ad5001/Functions/Main.php
@@ -105,15 +105,16 @@
                 $sender->sendMessage("§4§l[Functions]§r§4 Help for Function: \n- /function create <function>:§6 Create a function \n- /function ac <function> <command id> <command>:§6 Add a command to a function \n- /function rc <function> <command id> <command>:§6 Reset a command from a function\n- /function rmc <function> <command id> <command>:§6 Remove a command from a function\n- /function read <function>:§6 Read all commands of a function \n");
                 return true;
               break;
-			}
-			} else {
-				$sender->sendMessage("§4§l[Functions]§r§4 Help for Function: \n- /function create <function>:§6 Create a function \n- /function ac <function> <command id> <command>:§6 Add a command to a function \n- /function rc <function> <command id> <command>:§6 Reset a command from a function\n- /function rmc <function> <command id> <command>:§6 Remove a command from a function\n- /function read <function>:§6 Read all commands of a function \n");
-			}
-			return true;
-			break;
-  }
-  }
-  public function onCommandPreProcess(PlayerCommandPreprocessEvent $event) {
+            }
+          }else{
+            $sender->sendMessage("§4§l[Functions]§r§4 Help for Function: \n- /function create <function>:§6 Create a function \n- /function ac <function> <command id> <command>:§6 Add a command to a function \n- /function rc <function> <command id> <command>:§6 Reset a command from a function\n- /function rmc <function> <command id> <command>:§6 Remove a command from a function\n- /function read <function>:§6 Read all commands of a function \n");
+          }
+          return true;
+        break;
+      }
+    }
+
+    public function onCommandPreProcess(PlayerCommandPreprocessEvent $event){
 		    $cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
 			$args = $event->getMessage();
 			$sender = $event->getPlayer();
