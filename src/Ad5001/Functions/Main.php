1e3f7c08ecf3a7d518d9ae4dd2058ed722df8709
Ad5001 <Ad5001@users.noreply.github.com>
2017-01-22 19:52:16 +0100

    Resolved 0.16 command problems + Syntax errors !
    Everything should be fixed now!

--- src/Ad5001/Functions/Main.php
+++ src/Ad5001/Functions/Main.php
@@ -1,5 +1,4 @@
 <?php
-
   namespace Ad5001\Functions;
   use pocketmine\command\CommandSender;
   use pocketmine\command\Command;
@@ -9,17 +8,26 @@
   use pocketmine\command\Loader;
   use pocketmine\event\Listener;
   use pocketmine\Player;
-  use pocketmine\katana\Console;
   use pocketmine\server;
   use pocketmine\IPlayer;
   use pocketmine\utils\Config;
   use pocketmine\plugin\PluginBase;
-
   class Main extends PluginBase implements Listener{
 
+
     public function onEnable(){
-      $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->cmds = [];
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        foreach($this->getConfig()->getAll() as $cmd => $cmds) {
+            $cmd  = substr($cmd, 1); // Removing the "/"
+            $this->cmds[$cmd] = new \pocketmine\command\PluginCommand($name, $this);
+            $this->cmds[$cmd]->setUsage("/$cmd [arguments]");
+            $this->cmds[$cmd]->setDescription("Runs function $cmd.");
+            $this->cmds[$cmd]->register($this->getServer()->getCommandMap());
+        }
     }
+
+
 
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
       switch($command->getName()){
@@ -36,11 +44,14 @@
                   $cfg->set("/".$args[1], $default);
                   $cfg->save();
                   $this->reloadConfig();
+                  $this->cmds[$args[1]] = new \pocketmine\command\PluginCommand($name, $this);
+                  $this->cmds[$args[1]]->setUsage("/$cmd [arguments]");
+                  $this->cmds[$args[1]]->setDescription("Runs function $args[1].");
+                  $this->cmds[$args[1]]->register($this->getServer()->getCommandMap());
                   $sender->sendMessage("§4§l[Functions]§r§4 Function " . $args[1] . " has been created! You can edit it on the config or by doing /function ac <function> <command number> <command...>.");
                 }
                 return true;
               break;
-
               case "ac":
               case "addcmd":
                 $cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
@@ -59,7 +70,6 @@
                 }
                 return true;
               break;
-
               case "rc":
               case "resetcmd":
                 $cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
@@ -72,7 +82,6 @@
                 $this->reloadConfig();
                 return true;
               break;
-
               case "rmc":
               case "removecmd":
                 $cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
@@ -87,7 +96,6 @@
                 $sender->sendMessage("§4§l[Functions]§r§4 Removed command $args[2] from function $args[1]");
                 return true;
               break;
-
               case "read":
                 $cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
                 $i = 1;
@@ -100,75 +108,89 @@
                 }
                 return true;
               break;
-
+              case "delete":
+              case "remove":
+              case "del":
+              case "rm":
+                $cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+                $cfg->remove($args[1]);
+                $cfg->save();
+                $this->reloadConfig();
+                $this->cmds[$args[1]]->unregister($this->getServer()->getCommandMap());
+                unset($this->cmds[$args[1]]);
+                $sender->sendMessage("§4§l[Functions]§r§4 Succefully removed function " . $args[1] . ".");
+                return true;
+              break;
               default:
-                $sender->sendMessage("§4§l[Functions]§r§4 Help for Function: \n------------------------- \n- /function create <function>:§6 Create a function \n- /function ac <function> <command id> <command>:§6 Add a command to a function \n- /function rc <function> <command id> <command>:§6 Reset a command from a function\n- /function rmc <function> <command id> <command>:§6 Remove a command from a function\n- /function read <function>:§6 Read all commands of a function \n---------------------------\n");
+                $sender->sendMessage("§4§l[Functions]§r§4 Help for Function: \n------------------------- \n- /function create <function>:§6 Create a function \n- /function ac <function> <command id> <command>:§6 Add a command to a function \n- /function rc <function> <command id> <command>:§6 Reset a command from a function\n- /function rmc <function> <command id> <command>:§6 Remove a command from a function\n- /function read <function>:§6 Read all commands of a function \n- /function delete <function>:§6 Deletes a function \n---------------------------\n");
                 return true;
               break;
             }
           }else{
-            $sender->sendMessage("§4§l[Functions]§r§4 Help for Function: \n------------------------- \n- /function create <function>:§6 Create a function \n- /function ac <function> <command id> <command>:§6 Add a command to a function \n- /function rc <function> <command id> <command>:§6 Reset a command from a function\n- /function rmc <function> <command id> <command>:§6 Remove a command from a function\n- /function read <function>:§6 Read all commands of a function \n---------------------------\n");
+            $sender->sendMessage("§4§l[Functions]§r§4 Help for Function: \n------------------------- \n- /function create <function>:§6 Create a function \n- /function ac <function> <command id> <command>:§6 Add a command to a function \n- /function rc <function> <command id> <command>:§6 Reset a command from a function\n- /function rmc <function> <command id> <command>:§6 Remove a command from a function\n- /function read <function>:§6 Read all commands of a function \n- /function delete <function>:§6 Deletes a function \n---------------------------\n");
           }
           return true;
         break;
       }
     }
 
+
+
     public function onCommandPreProcess(PlayerCommandPreprocessEvent $event){
-      $cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
-      $args = $event->getMessage();
-      $sender = $event->getPlayer();
-      $args = explode(" ", $args);
-      $cmds = $cfg->get($args[0]);
-      if(is_array($cmds)){
-        $funcname = $args[0];
-        unset($args[0]);
-        if($sender->isPermissionSet("func.use." . $funcname) ? $sender->hasPermission("func.use." . $funcname) : $sender->hasPermission("func.use.default")){
-          foreach($cmds as $cmd){
-            if($cmd !== "nothink"){
-              $cmd = str_ireplace("{sender}", $sender->getName(), $cmd);
-              $cmd = str_ireplace("{level}", $sender->getLevel()->getName(), $cmd);
-              $cmd = str_ireplace("{x}", $sender->x, $cmd);
-              $cmd = str_ireplace("{y}", $sender->y, $cmd);
-              $cmd = str_ireplace("{z}", $sender->z, $cmd);
-              $cmd = str_ireplace("{yaw}", $sender->yaw, $cmd);
-              $cmd = str_ireplace("{pitch}", $sender->pitch, $cmd);
-              if(!isset($args[1])){
-                $cmd = str_ireplace("{args[0]}", "", $cmd);
-              }
-              $cmd = str_ireplace("{args[0]}", $args[1], $cmd);
-              if(!isset($args[2])){
-                $cmd = str_ireplace("{args[1]}", "", $cmd);
-              }
-              $cmd = str_ireplace("{args[1]}", $args[2], $cmd);
-              $cmd = str_ireplace("{args}", $args, $cmd);
-              if(!isset($args[3])){
-                $cmd = str_ireplace("{args[2]}", "", $cmd);
-              }
-              $cmd = str_ireplace("{args[2]}", $args[3], $cmd);
-              if(!isset($args[4])){
-                $cmd = str_ireplace("{args[3]}", $args[4], $cmd);
-              }
-              if($cmd === "tell " . $sender->getName() . " This is default command, modify it with /function setc <function> <Command number> <command...>"){
-                $this->getServer()->dispatchCommand(new ConsoleCommandSender(), $cmd);
-              }elseif(strpos($cmd, "{console}")){
-                $cmd = str_ireplace("{console}", "", $cmd);
-                $this->getServer()->dispatchCommand(new ConsoleCommandSender(), $cmd);
-              }elseif(strpos($cmd, "{op}")){
-                $cmd = str_ireplace("{op}", "", $cmd);
-                if ($sender->isOp()){
-                  $this->getServer()->dispatchCommand($sender, $cmd);
-                } else {
-                  $sender->setOp(true);
-                  $this->getServer()->dispatchCommand($sender, $cmd);
-                  $sender->setOp(false);
-                }
+        $cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+        $args = $event->getMessage();
+        $sender = $event->getPlayer();
+        $args = explode(" ", $args);
+        $cmds = $cfg->get($args[0]);
+        if(is_array($cmds)){
+            $funcname = $args[0];
+            unset($args[0]);
+            if($sender->isPermissionSet("func.use." . $funcname) ? $sender->hasPermission("func.use." . $funcname) : $sender->hasPermission("func.use.default")){
+            foreach($cmds as $cmd){
+                if($cmd !== "nothink"){
+                    $cmd = str_ireplace("{sender}", $sender->getName(), $cmd);
+                    $cmd = str_ireplace("{level}", $sender->getLevel()->getName(), $cmd);
+                    $cmd = str_ireplace("{x}", $sender->x, $cmd);
+                    $cmd = str_ireplace("{y}", $sender->y, $cmd);
+                    $cmd = str_ireplace("{z}", $sender->z, $cmd);
+                    $cmd = str_ireplace("{yaw}", $sender->yaw, $cmd);
+                    $cmd = str_ireplace("{pitch}", $sender->pitch, $cmd);
+                    if(!isset($args[1])){
+                        $cmd = str_ireplace("{args[0]}", "", $cmd);
+                    }
+                    $cmd = str_ireplace("{args[0]}", $args[1], $cmd);
+                    if(!isset($args[2])){
+                        $cmd = str_ireplace("{args[1]}", "", $cmd);
+                    }
+                    $cmd = str_ireplace("{args[1]}", $args[2], $cmd);
+                    $cmd = str_ireplace("{args}", $args, $cmd);
+                    if(!isset($args[3])){
+                        $cmd = str_ireplace("{args[2]}", "", $cmd);
+                    }
+                    $cmd = str_ireplace("{args[2]}", $args[3], $cmd);
+                    if(!isset($args[4])){
+                        $cmd = str_ireplace("{args[3]}", $args[4], $cmd);
+                    }
+                    if($cmd === "tell " . $sender->getName() . " This is default command, modify it with /function setc <function> <Command number> <command...>"){
+                        $this->getServer()->dispatchCommand(new ConsoleCommandSender(), $cmd);
+                    }elseif(strpos($cmd, "{console}")){
+                        $cmd = str_ireplace("{console}", "", $cmd);
+                        $this->getServer()->dispatchCommand(new ConsoleCommandSender(), $cmd);
+                    }elseif(strpos($cmd, "{op}")){
+                        $cmd = str_ireplace("{op}", "", $cmd);
+                    if ($sender->isOp()){
+                        $this->getServer()->dispatchCommand($sender, $cmd);
+                    } else {
+                        $sender->setOp(true);
+                        $this->getServer()->dispatchCommand($sender, $cmd);
+                        $sender->setOp(false);
+                    }
               }else{
-                $this->getServer()->dispatchCommand($sender, $cmd);
+                    $this->getServer()->dispatchCommand($sender, $cmd);
               }
             }
           }
-        else {
+        } else {
           $sender->sendMessage("You do not have permission to use this function.");
         }
         $event->setCancelled();
