fec79be08f830bc4f09649bda9e69b9bdd02fa78
Ulti <Ultie0@users.noreply.github.com>
2017-03-11 08:27:28 +0300

    *sigh*...

--- jenkins.php
+++ jenkins.php
@@ -1,4 +1,4 @@
-<?php
+(<?php
 $server = proc_open(PHP_BINARY . " src/pocketmine/PocketMine.php --no-wizard", [
 //$server = proc_open("./start.sh --no-wizard", [
 	0 => ["pipe", "r"],
@@ -18,8 +18,8 @@ fclose($pipes[2]);
 echo "\n\nReturn value: ". proc_close($server) ."\n";
 if(count(glob("crashdumps/CrashDump*.log")) === 0){
 	echo "The functions plugin is currently working well.\nOur jenkins system doesn't currently build phar files.\nTo download the plugin, get it from source.\n";
-	exit 0;
+	exit(0);
 }else{
 	echo "The functions plugin has a syntax error.\nIt will be fixed whenever the developers have a chance.\nPlease be patient and wait for them to fix it.\n";
-	exit 1;
+	exit(1);
 }
